8b3080ffa0c9cedd871ba3a960d997076bbf0ef4
bartoluzuzazo <bartbo62@gmail.com>
2024-08-07 12:07:59 +0200

    moved cors origins to appsettings.json

--- backend/MediaVerse/MediaVerse.Client.Api/Program.cs
+++ backend/MediaVerse/MediaVerse.Client.Api/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: local,policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "https://localhost:3000")
+        policy.WithOrigins(builder.Configuration["CORS:http"], builder.Configuration["CORS:https"])
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
