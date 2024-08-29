45b971b631033948a186fcb7d260ad1595752c15
Adam Jurkiewicz <s25467@pjwstk.edu.pl>
2024-08-29 17:08:32 +0200

    Switch to loading the entry with React query

--- frontend/src/routes/entries/books/$id.tsx
+++ frontend/src/routes/entries/books/$id.tsx
@@ -1,25 +1,36 @@
 import { createFileRoute } from '@tanstack/react-router'
 import EntryBanner from '../../../common/components/entries/entryBanner.tsx'
-import { Book } from '../../../models/entry/book/Book.ts'
 import EntrySectionHeader from '../../../common/components/entries/entrySectionHeader.tsx'
+import { useQueryClient, useQuery } from '@tanstack/react-query'
 import { bookService } from '../../../services/bookService.ts'
+import { Book } from '../../../models/entry/book/Book.ts'
 
 export const Route = createFileRoute('/entries/books/$id')({
-  loader: async ({ params }) => {
-    const response = await bookService.getBook(params.id)
-    return response.data
-  },
-
   component: () => {
-    const book = Route.useLoaderData<Book>()
+    // const queryClient = useQueryClient()
+    const { id } = Route.useParams()
+
+    const { data: book, isLoading } = useQuery({
+      queryFn: async (): Promise<Book> => {
+        const res = await bookService.getBook(id)
+        return res.data
+      },
+      queryKey: ['GET_BOOK'],
+    })
+    if (isLoading) {
+      return <div>Loading...</div>
+    }
+
+    if (book == undefined) {
+      return <div>An impossible error occured</div>
+    }
+
     const info = [book.entry.release.toString(), ...book.bookGenres]
     return (
       <>
         <EntryBanner entry={book.entry} info={info} type={'Book'} />
         <EntrySectionHeader title={'Description'} />
-        <div className="p-4">
-          {book.entry.description}
-        </div>
+        <div className="p-4">{book.entry.description}</div>
         <EntrySectionHeader title={'Authors'} />
       </>
     )
--- frontend/src/services/bookService.ts
+++ frontend/src/services/bookService.ts
@@ -1,10 +1,11 @@
 import axios from 'axios'
+import { Book } from '../models/entry/book/Book.ts'
 
 export class bookService {
   public static async getBook(id: string) {
-    return await axios.get('book', {
+    return await axios.get<Book>('book', {
       params: {
-        'id': id,
+        id: id,
       },
     })
   }
