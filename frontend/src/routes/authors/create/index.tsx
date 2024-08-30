ad5ea831fcee3acba3f075ad7381630125c57a8e
Adam Jurkiewicz <s25467@pjwstk.edu.pl>
2024-08-30 21:27:10 +0200

    Remove debugging code

--- frontend/src/routes/authors/create/index.tsx
+++ frontend/src/routes/authors/create/index.tsx
@@ -1,14 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import AuthorForm from '../../../common/components/authors/AuthorForm.tsx'
-import { useEffect } from 'react'
-import axios from 'axios'
 
 export const Route = createFileRoute('/authors/create/')({
   component: () => {
-    useEffect(() => {
-      axios.get('/book/me').then((res) => console.log(res.data))
-    }, [])
-
     return <AuthorForm />
   },
 })
