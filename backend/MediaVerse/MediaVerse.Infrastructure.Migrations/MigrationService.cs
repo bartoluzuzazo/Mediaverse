becac54bf9824627cf8bc84160d7c81c3919c382
mWieczorek02 <mikwie9@gmail.com>
2024-09-03 19:45:24 +0200

    add DbUp

---
+++ backend/MediaVerse/MediaVerse.Infrastructure.Migrations/MigrationService.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using DbUp;
+using Serilog;
+
+namespace MediaVerse.Infrastructure.Migrations;
+
+public class MigrationService(string connectionString)
+{
+    public void Migrate()
+    {
+        var upgradeEngine = DeployChanges.To.PostgresqlDatabase(connectionString)
+            .WithScriptsEmbeddedInAssembly(typeof(MigrationService).GetTypeInfo().Assembly)
+            .LogTo(new UpgradeLogger())
+            .Build();
+
+        if (upgradeEngine is null)
+        {
+            Log.Error("MigrationService has not been deployed");
+            return;
+        }
+        
+        var result = upgradeEngine.PerformUpgrade();
+
+        if (!result.Successful)
+        {
+            Log.Error("Migration failed:\n {0}", result.Error.ToString());
+        }
+        else
+        {
+            Log.Information("Migration completed successfully");
+        }
+    }
+}
---
+++ backend/MediaVerse/MediaVerse.Infrastructure.Migrations/UpgradeLogger.cs
@@ -0,0 +1,19 @@
+﻿using DbUp.Engine.Output;
+using Serilog;
+
+namespace MediaVerse.Infrastructure.Migrations;
+
+public class UpgradeLogger : IUpgradeLog
+{
+    public void LogError(string format, params object[] args) => Log.Error(format, args);
+
+    public void LogError(Exception ex, string format, params object[] args) => Log.Error(ex, format, args);
+
+    public void LogWarning(string format, params object[] args) => Log.Warning(format, args);
+
+    public void LogTrace(string format, params object[] args) => Log.Information(format, args);
+
+    public void LogDebug(string format, params object[] args) => Log.Debug(format, args);
+
+    public void LogInformation(string format, params object[] args) => Log.Information(format, args);
+}
