e476caad1272f5784eed42bbba7ea6861a846040
bartoluzuzazo <bartbo62@gmail.com>
2024-10-14 18:29:12 +0200

    add entry type triggers

---
+++ backend/MediaVerse/MediaVerse.Infrastructure.Migrations/Files/2024/10/002_AddTypeTriggers.sql
@@ -0,0 +1,55 @@
+CREATE OR REPLACE FUNCTION set_entry_type() RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS 
+$$
+BEGIN
+    UPDATE entry SET type = tg_argv[0]::text
+    WHERE id = new.id;
+    RETURN new;
+END;
+$$;
+
+CREATE OR REPLACE TRIGGER book_type_trigger
+    AFTER INSERT OR UPDATE ON book
+                                  FOR EACH ROW EXECUTE PROCEDURE set_entry_type('Book');
+
+CREATE OR REPLACE TRIGGER movie_type_trigger
+    AFTER INSERT OR UPDATE ON movie
+                                  FOR EACH ROW EXECUTE PROCEDURE set_entry_type('Movie');
+
+CREATE OR REPLACE TRIGGER series_type_trigger
+    AFTER INSERT OR UPDATE ON series
+                                  FOR EACH ROW EXECUTE PROCEDURE set_entry_type('Series');
+
+CREATE OR REPLACE TRIGGER episode_type_trigger
+    AFTER INSERT OR UPDATE ON episode
+                                  FOR EACH ROW EXECUTE PROCEDURE set_entry_type('Episode');
+
+CREATE OR REPLACE TRIGGER game_type_trigger
+    AFTER INSERT OR UPDATE ON game
+                                  FOR EACH ROW EXECUTE PROCEDURE set_entry_type('Game');
+
+CREATE OR REPLACE TRIGGER song_type_trigger
+    AFTER INSERT OR UPDATE ON song
+                                  FOR EACH ROW EXECUTE PROCEDURE set_entry_type('Song');
+
+CREATE OR REPLACE TRIGGER album_type_trigger
+    AFTER INSERT OR UPDATE ON album
+                                  FOR EACH ROW EXECUTE PROCEDURE set_entry_type('Album');
+
+
+CREATE OR REPLACE FUNCTION lock_entry_type() RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    IF new.type <> old.type THEN
+        RAISE EXCEPTION 'Entry type cannot be changed manually';
+END IF;
+RETURN new;
+END;
+$$;
+
+CREATE OR REPLACE TRIGGER entry_type_trigger
+    AFTER INSERT OR UPDATE ON entry
+                                  FOR EACH ROW EXECUTE PROCEDURE lock_entry_type();
