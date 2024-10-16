5c4253ed2c8b7050ffc1f4ab2a139993a8e9ec17
bartoluzuzazo <bartbo62@gmail.com>
2024-10-16 20:12:53 +0200

    fix ports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,10 +6,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8081',
       },
     },
   },
