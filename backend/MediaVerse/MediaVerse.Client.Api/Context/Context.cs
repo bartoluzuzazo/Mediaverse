a92e84d04c1621e6b665f25b4ba657f5f8176147
Adam Jurkiewicz <s25467@pjwstk.edu.pl>
2024-10-29 21:02:19 +0100

    Fix db scaffold

--- backend/MediaVerse/MediaVerse.Client.Api/Context/Context.cs
+++ backend/MediaVerse/MediaVerse.Client.Api/Context/Context.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using MediaVerse.Client.Api.Entities;
+using MediaVerse.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace MediaVerse.Client.Api.Context;
