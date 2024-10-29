f8d681a138483a375617817543b2e2a89af6adf2
bartoluzuzazo <bartbo62@gmail.com>
2024-10-29 20:12:44 +0100

    fix missing props

--- frontend/src/routes/users/search.tsx
+++ frontend/src/routes/users/search.tsx
@@ -1,7 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { UserSearch } from '../../common/components/users/UserSearch'
+import { userService } from '../../services/userService.ts'
 
 
 export const Route = createFileRoute('/users/search')({
-	component: ()=><div className="mt-6"><UserSearch/></div>
+	component: ()=><div className="mt-6"><UserSearch queryKey={"SEARCH_USERS"} searchFunction={userService.search}/></div>
 })
