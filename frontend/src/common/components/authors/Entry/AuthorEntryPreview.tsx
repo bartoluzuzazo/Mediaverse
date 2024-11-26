bb3beeed57b6c37b835fc0ee97d9681c279725ff
mWieczorek02 <mikwie9@gmail.com>
2024-11-26 20:42:56 +0100

    fix rounding

--- frontend/src/common/components/authors/Entry/AuthorEntryPreview.tsx
+++ frontend/src/common/components/authors/Entry/AuthorEntryPreview.tsx
@@ -3,22 +3,25 @@ import { AuthorEntry } from '../../../../models/author/Author.ts'
 import EntryType from '../../entries/entryType.tsx'
 import { useNavigate } from '@tanstack/react-router'
 
-interface Props{
+interface Props {
   entry: AuthorEntry
 }
 
-const AuthorEntryPreview : FunctionComponent<Props> = ({entry}) => {
+const AuthorEntryPreview: FunctionComponent<Props> = ({ entry }) => {
   const imgSrc = 'data:image/*;base64,' + entry.coverPhoto
-  const maxDescLen = 400;
-  const navigate = useNavigate();
+  const maxDescLen = 400
+  const navigate = useNavigate()
   const handleLink = async () => {
     await navigate({ to: `/entries/books/${entry.id}` })
   }
   return (
-    <div className="flex flex-row justify-between cursor-pointer" onClick={handleLink}>
+    <div
+      className="flex cursor-pointer flex-row justify-between"
+      onClick={handleLink}
+    >
       <img
         src={imgSrc}
-        className="aspect-square w-36 h-36 border-4 border-white bg-slate-300 p-2"
+        className="aspect-square h-36 w-36 border-4 border-white bg-slate-300 p-2"
         alt="cover photo"
       />
       <div className="flex flex-col p-2 font-bold">
@@ -26,14 +29,16 @@ const AuthorEntryPreview : FunctionComponent<Props> = ({entry}) => {
         <EntryType type={entry.type} />
         <div className="flex flex-row">
           <h2 className="p-2">{entry.releaseDate.toString()}</h2>
-          <h2 className="p-2">{entry.avgRating}★</h2>
+          <h2 className="p-2">{entry.avgRating.toFixed(2)}★</h2>
         </div>
       </div>
-      <p className="p-2 w-8/12">
-        {entry.description.length > maxDescLen ? entry.description.substring(0, maxDescLen)+" [...]" : entry.description}
+      <p className="w-8/12 p-2">
+        {entry.description.length > maxDescLen
+          ? entry.description.substring(0, maxDescLen) + ' [...]'
+          : entry.description}
       </p>
     </div>
   )
 }
 
-export default AuthorEntryPreview;
+export default AuthorEntryPreview
