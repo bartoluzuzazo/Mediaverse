8004a5db48f53a535e76eb2e17adb60d33872a8c
mWieczorek02 <mikwie9@gmail.com>
2024-12-24 11:53:55 +0100

    add user tests

---
+++ backend/MediaVerse/MediaVerse.Tests/Tests/User/GetUserTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using MediaVerse.Client.Application.Queries.UserQueries;
+
+namespace MediaVerse.Tests.Tests.User;
+
+[Collection("ClientHost")]
+public class GetUserTests : MediaVerseTestBase
+{
+    public GetUserTests(ClientHostFixture clientHostFixture) : base(clientHostFixture)
+    {
+    }
+
+    [Fact]
+    public async Task GetUser_Should_ReturnNotFoundForNonExistingUser()
+    {
+        // Arrange
+        var client = Factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/api/user/af7c1fe6-d669-4000-0000-e9733f0de7a8");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task GetUser_Should_ReturnSuccessForExistingUser()
+    {
+        // Arrange
+        var client = Factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/api/user/af7c1fe6-d669-414e-b066-e9733f0de7a8");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+    }
+}
---
+++ backend/MediaVerse/MediaVerse.Tests/Tests/User/LoginTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using MediaVerse.Client.Application.Commands.UserCommands;
+using MediaVerse.Client.Application.Queries.UserQueries;
+
+namespace MediaVerse.Tests.Tests.User;
+
+[Collection("ClientHost")]
+public class LoginTests : MediaVerseTestBase
+{
+    public LoginTests(ClientHostFixture clientHostFixture) : base(clientHostFixture)
+    {
+    }
+
+    [Fact]
+    public async Task Login_Should_ReturnNotFoundForNonExistingUser()
+    {
+        // Arrange
+        var client = Factory.CreateClient();
+
+        // Act
+        var response = await client.PostAsJsonAsync("/api/user/login", new LoginUserQuery()
+        {
+            Email = "non-exisiting@admin.com",
+            Password = "admin",
+        });
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Login_Should_ReturnNotFoundForWrongPassword()
+    {
+        // Arrange
+        var client = Factory.CreateClient();
+
+        // Act
+        var response = await client.PostAsJsonAsync("/api/user/login", new LoginUserQuery()
+        {
+            Email = "admin@admin.com",
+            Password = "wrong-password",
+        });
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Login_Should_ReturnSuccessForCorrectCredentials()
+    {
+        // Arrange
+        var client = Factory.CreateClient();
+
+        // Act
+        var response = await client.PostAsJsonAsync("/api/user/login", new LoginUserQuery()
+        {
+            Email = "admin@admin.com",
+            Password = "admin",
+        });
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+    }
+}
