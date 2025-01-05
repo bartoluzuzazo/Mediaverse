123e4521af9c255b0a790c03b9eaaa8f006834e8
bartoluzuzazo <bartbo62@gmail.com>
2025-01-05 20:14:24 +0100

    add title for entries on carousel

--- frontend/src/common/components/entries/entryCarousel.tsx
+++ frontend/src/common/components/entries/entryCarousel.tsx
@@ -21,13 +21,16 @@ const EntryCarousel: FunctionComponent<EntryCarouselProps> = ({
     <div>
       <p className="select-none text-2xl font-bold">{title}</p>
       <CustomCarousel>
-        {entries.map(({ id, photo}) => (
+        {entries.map(({ id, photo, name}) => (
           <Link
             key={id}
             className="m-1 my-3 block h-[235px] w-[170px] overflow-hidden transition-shadow"
             to="/entries/$id"
             params={{ id }}
           >
+            <div className="absolute top-1/2 p-3 text-center center text-white bg-black w-[170px] h-1/4 bg-opacity-30 align-middle pointer-events-none">
+              {name}
+            </div>
             <CustomImage
               className="h-full w-full object-cover transition-all hover:scale-[1.1]"
               src={`data:image/webp;base64,${photo}`}
